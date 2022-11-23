c4d52628057482c633754a4ab60ffd498dbc6eb3
Ultra-tech-code <hizick27@gmail.com>
2022-11-23 09:17:53 +0100

    implemeted new functions

--- contracts/uber.sol
+++ contracts/uber.sol
@@ -1,21 +1,23 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
-
-contract Uber {
-
+contract uberDrive{
     address owner;
 
     constructor() {
         owner == msg.sender;
     }
 
+    modifier onlyOwner(){
+        require(msg.sender == owner, "not an admin");
+        _;
+    }
+
     struct driverDetails{
         address driversAddress;
         string driversName;
         bool registered;
+        bool approved;
         bool booked;
         bool arrived;
         uint timePicked;
@@ -30,6 +32,7 @@ contract Uber {
     }
 
     address[] driversAddress;
+    address[] driverReviewers;
 
     mapping(address => driverDetails) driverdetails;
     mapping(address => riderDetails) riderdetails;
@@ -46,7 +49,13 @@ contract Uber {
         dd.driversAddress = msg.sender;
         dd.driversName = _drivername;
         dd.registered = true;
+        dd.approved = false;
         driversAddress.push(msg.sender);
+    }
+
+    function reviewDriver(address driversAddress) public{
+        driverDetails storage dd = driverdetails[driversAddress];
+        dd.approved = true;
     }
 
 
@@ -67,8 +76,42 @@ contract Uber {
     function pickRide() public {
         driverDetails storage dd = driverdetails[msg.sender];
         require(dd.registered == true, "not registered");
+        require(dd.approved == true, "approval still pendind");
         require(dd.booked == false, "already booked");
+        dd.timePicked = block.timestamp;
+        dd.booked = true;
+        
+    }
+
+    function payFee () public {
+        riderDetails storage rd = riderdetails[msg.sender];
 
     }
-  
+
+    function endride() public{
+        driverDetails storage dd = driverdetails[msg.sender];
+        require(dd.booked == true, "you have no active ride");
+        dd.timeDestination = block.timestamp;
+        dd.booked = false;
+
+        uint amount = calcFee();
+    }
+
+    function addReviewers(address reviewersAddress) public onlyOwner{
+        driverReviewers.push(reviewersAddress);
+    }
+
+    function calcFee() internal returns(uint256){
+        driverDetails storage dd = driverdetails[msg.sender];
+        uint timepicked = dd.timePicked;
+        uint timereach = dd.timeDestination;
+        uint totalTime = timereach - timepicked;
+
+        uint amountToPay = totalTime * 2;
+
+        return amountToPay;
+
+    }
+
+    
 }
